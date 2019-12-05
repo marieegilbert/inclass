aec49946de3c4776fd6c0a2823ed787b0a54b0d9
michellesokolov <michellersokolov@gmail.com>
2019-12-04 21:22:12 -0800

    updated server

--- server.js
+++ server.js
@@ -23,50 +23,48 @@ var waitList = [
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     // res.send("Hot Restaurant")
     res.sendFile(path.join(__dirname, "index.html"));
-  });
+});
+//condtional statement. if guestlist has 5 guests, new guests will be moved to waitlist.
 
 // Create New guests - takes in JSON input
-app.post("/api/guests", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  var newGuest = req.body;
+app.post("/api/reserve", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    var newGuest = req.body;
 
-  console.log(newGuest);
+    console.log(newGuest);
 
-  // We then add the json the user sent to the guestList array
-  guestList.push(newGuest);
+    if (guestList.length < 4) {
+        // We then add the json the user sent to the guestList array
+        guestList.push(newGuest);
+        // We then display the JSON to the users
+        res.json(newGuest);
+        //});
+    }
+    else {
+        // Create New waitlisted guests - takes in JSON input
 
-  // We then display the JSON to the users
-  res.json(newGuest);
+        // We then add the json the user sent to the waitList array
+        waitList.push(newGuest);
+
+        // We then display the JSON to the users
+        res.json(newGuest);
+        // });
+    }
+})
+
+// Displays all guests
+app.get("/api/guestList", function (req, res) {
+    return res.json(guestList);
 });
 
-// Create New waitlisted guests - takes in JSON input
-app.post("/api/waitListGuests", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    var newwaitListGuest = req.body;
-  
-    console.log(newwaitListGuest);
-  
-    // We then add the json the user sent to the waitList array
-    waitList.push(newwaitListGuest);
-  
-    // We then display the JSON to the users
-    res.json(newwaitListGuest);
-  });
-
-  
-  // Displays all guests
-  app.get("/api/guestList", function(req, res) {
-    return res.json(guestList);
-  });
-
-  // Displays all waitlist guests
-  app.get("/api/waitlist", function(req, res) {
+// Displays all waitlist guests
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitList);
-  });
+});
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
